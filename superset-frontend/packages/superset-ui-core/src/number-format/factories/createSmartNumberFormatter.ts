183a7c2bc546bb6e3b9a6f68847bce40f078cb49
silpher1234 <134906768+silpher1234@users.noreply.github.com>
2023-06-12 15:05:48 +0530

    Update createSmartNumberFormatter.ts

--- superset-frontend/packages/superset-ui-core/src/number-format/factories/createSmartNumberFormatter.ts
+++ superset-frontend/packages/superset-ui-core/src/number-format/factories/createSmartNumberFormatter.ts
@@ -24,54 +24,27 @@ import NumberFormats from '../NumberFormats';
 const siFormatter = d3Format(`.3~s`);
 const float2PointFormatter = d3Format(`.2~f`);
 const float4PointFormatter = d3Format(`.4~f`);
-
 function formatValue(value: number) {
   if (value === 0) {
     return '0';
   }
   const absoluteValue = Math.abs(value);
-  if (absoluteValue >= 10000000) {
-    // Convert to crores (Cr)
-    return `${float2PointFormatter(value / 10000000)} Crore`;
-  }
-  if (absoluteValue >= 100000) {
-    // Convert to lakhs (L)
-    return `${float2PointFormatter(value / 100000)} Lakh`;
-  }
   if (absoluteValue >= 1000) {
-    // Convert to thousands (k)
-    return `${float2PointFormatter(value / 1000)}k`;
+    // Normal human being are more familiar
+    // with billion (B) that giga (G)
+    return siFormatter(value).replace('G', 'B');
+  }
+  if (absoluteValue >= 1) {
+    return float2PointFormatter(value);
   }
   if (absoluteValue >= 0.001) {
     return float4PointFormatter(value);
   }
   if (absoluteValue > 0.000001) {
-    return `${float2PointFormatter(value * 1000000)} µ`;
+    return `${siFormatter(value * 1000000)}µ`;
   }
-  return float2PointFormatter(value);
+  return siFormatter(value);
 }
-
-// function formatValue(value: number) {
-//   if (value === 0) {
-//     return '0';
-//   }
-//   const absoluteValue = Math.abs(value);
-//   if (absoluteValue >= 1000) {
-//     // Normal human being are more familiar
-//     // with billion (B) that giga (G)
-//     return siFormatter(value).replace('G', 'B');
-//   }
-//   if (absoluteValue >= 1) {
-//     return float2PointFormatter(value);
-//   }
-//   if (absoluteValue >= 0.001) {
-//     return float4PointFormatter(value);
-//   }
-//   if (absoluteValue > 0.000001) {
-//     return `${siFormatter(value * 1000000)}µ`;
-//   }
-//   return siFormatter(value);
-// }
 
 export default function createSmartNumberFormatter(
   config: {
